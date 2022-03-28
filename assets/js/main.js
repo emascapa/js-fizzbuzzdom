1afad7da951ba06f1c34fcfe4c27991643057d08
emascapa <emascapa@gmail.com>
2022-03-28 17:38:08 +0200

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,63 @@ const buzz = "buzz";
 
 //ciclo for per stampare i numeri
 for (let i = 1; i <= 100; i++) {
+
+    console.log(i);
+    console.log('eccolo');
+    
+    // elementRow.innerHTML = elementRow.innerHTML + elementColumn;
+
+    //se il numero è multiplo di 3 e 5 comparirà fizzbuzz
+    if (i % 3 === 0 && i % 5 === 0) {
+        elementColumn = `
+        <div class="col d-flex justify-content-center">
+            <div class="squared bg_fizzbuzz">
+            fizzbuzz
+            </div>
+        </div>
+        `;
+    }
+    //se il numero è multiplo di 3 ma non di 5 comparirà fizz 
+    else if (i % 3 === 0 && i % 5 !== 0) {
+        elementColumn = `
+        <div class="col d-flex justify-content-center">
+            <div class="squared bg_fizz">
+            fizz
+            </div>
+        </div>
+        `;
+    }
+    //se il numero è multiplo di 5 ma non di 3 comparirà buzz 
+    else if (i % 3 !== 0 && i % 5 === 0) {
+        elementColumn = `
+        <div class="col d-flex justify-content-center">
+            <div class="squared bg_buzz">
+            buzz
+            </div>
+        </div>
+        `;
+    }
+    //se il numero non rispetta le condizioni precedenti stamperà il numero di turno 
+    else {
+        //console.log(document.querySelector(`.squared-${i}`).classList);
+        elementColumn = `
+        <div class="col d-flex justify-content-center">
+            <div class="squared bg_number">
+            ${i}
+            </div>
+        </div>
+        `;
+    }
+
+    //aggiungo l'elemento contenitore alla riga
+    elementRow.insertAdjacentHTML('beforeend', elementColumn)
+}
+
+
+
+/* 
+//ciclo for per stampare i numeri
+for (let i = 1; i <= 100; i++) {
     elementColumn = `
     <div class="col">
         <div class="squared-${i}">
@@ -30,23 +87,23 @@ for (let i = 1; i <= 100; i++) {
 
     //se il numero è multiplo di 3 e 5 comparirà fizzbuzz
     if (i % 3 === 0 && i % 5 === 0) {
-        document.querySelector(`squared-${i}`).classList.add(bg_fizzbuzz);
-        document.querySelector(`squared-${i}`).insertAdjacentElement('beforeend', fizzBuzz);
+        document.querySelector(`.squared-${i}`).classList.add(bg_fizzbuzz);
+        document.querySelector(`.squared-${i}`).insertAdjacentElement('beforeend', fizzBuzz);
     }
     //se il numero è multiplo di 3 ma non di 5 comparirà fizz 
     else if (i % 3 === 0 && i % 5 !== 0) {
-        document.querySelector(`squared-${i}`).classList.add(bg_fizz);
-        document.querySelector(`squared-${i}`).insertAdjacentElement('beforeend', fizz);
+        document.querySelector(`.squared-${i}`).classList.add(bg_fizz);
+        document.querySelector(`.squared-${i}`).insertAdjacentElement('beforeend', fizz);
     }
     //se il numero è multiplo di 5 ma non di 3 comparirà buzz 
     else if (i % 3 !== 0 && i % 5 === 0) {
-        document.querySelector(`squared-${i}`).classList.add(bg_buzz);
-        document.querySelector(`squared-${i}`).insertAdjacentElement('beforeend', buzz);
+        document.querySelector(`.squared-${i}`).classList.add(bg_buzz);
+        document.querySelector(`.squared-${i}`).insertAdjacentElement('beforeend', buzz);
     }
     //se il numero non rispetta le condizioni precedenti stamperà il numero di turno 
     else {
-        console.log(document.querySelector(`squared-${i}`).classList);
-        //document.querySelector(`squared-${i}`).classList.add('bg_number');
-        //document.querySelector(`squared-${i}`).insertAdjacentElement('beforeend', i);
+        //console.log(document.querySelector(`.squared-${i}`).classList);
+        document.querySelector(`.squared-${i}`).classList.add('bg_number');
+        document.querySelector(`.squared-${i}`).insertAdjacentElement('beforeend', `${i}`);
     }
-}
+} */
